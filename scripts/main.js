8ce9115c349e12712c3a4e37f3c1014927c26144
Matthew C. Miller <mattymil2@gmail.com>
2014-03-09 20:56:52 -0400

    finalization

--- scripts/main.js
+++ scripts/main.js
@@ -6,17 +6,47 @@
 
 function scrollNext(iter) {
     this.iter = iter;
-
+    hideELs();
     if (iter == 1) {
-        $('#veil_message_stopper').ScrollTo({
+        $('#veil_title').ScrollTo({
             duration: 2000,
             easing: 'swing',
             callback: function () {
-                alert("callback executed");
+                showMe();
             }
         });
-    } else
+    } else if (iter == 2) {
+        $('#rsvp').ScrollTo({
+            duration: 2000,
+            easing: 'swing',
+            callback: function () {
+                showMe();
+            }
+        }); }else
         alert("you didn't give me the right value");
 }
 
+function showMe(){
+    $(function () {
+        $("#slides").slidesjs({
+            width: 600,
+            height: 599,
 
+            play: {
+                active: false,
+                effect: "slide",
+                interval: 4000,
+                auto: true,
+                swap: false,
+                pauseOnHover: false,
+                restartDelay: 2500
+            }
+
+
+        });
+    });
+}
+
+function hideELs(){
+    $("#hider").removeClass("hidden");
+}
